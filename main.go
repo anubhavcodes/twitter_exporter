e69af876f15fa8eeb94878ee879754788d015f23
Camal Cakar <camal.cakar@jimdo.com>
2021-04-07 21:23:20 +0200

    Go Fmt;

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ var (
 )
 
 func recordMetrics() {
-	go func(){
+	go func() {
 		for {
 			err, body := getTwitterData()
 			if err != nil {
@@ -50,7 +50,6 @@ func recordMetrics() {
 	}()
 }
 
-
 func main() {
 	recordMetrics()
 	http.Handle("/metrics", promhttp.Handler())
@@ -61,7 +60,7 @@ func main() {
 func getTwitterData() (error, []byte) {
 	var TwitterToken = os.Getenv("TWITTER_TOKEN")
 	var TwitterHandle = os.Getenv("TWITTER_HANDLE")
-	if TwitterToken == "" || TwitterHandle == ""{
+	if TwitterToken == "" || TwitterHandle == "" {
 		log.Println("Please define TWITTER_TOKEN and the TWITTER_HANDLE environment variable")
 		os.Exit(100)
 	}
@@ -86,20 +85,20 @@ type APIResponse struct {
 }
 
 type Data struct {
-	Id string `json:"id"`
-	Name string `json:"name"`
-	Username string `json:"username"`
+	Id            string        `json:"id"`
+	Name          string        `json:"name"`
+	Username      string        `json:"username"`
 	PublicMetrics PublicMetrics `json:"public_metrics"`
 }
 
 type PublicMetrics struct {
 	FollowersCount int `json:"followers_count"`
 	FollowingCount int `json:"following_count"`
-	TweetCount int `json:"tweet_count"`
-	ListedCount int `json:"listed_count"`
+	TweetCount     int `json:"tweet_count"`
+	ListedCount    int `json:"listed_count"`
 }
 
-func getPublicMetrics(apiResponse []byte) PublicMetrics  {
+func getPublicMetrics(apiResponse []byte) PublicMetrics {
 	var response APIResponse
 	if err := json.Unmarshal(apiResponse, &response); err != nil {
 		log.Println(err)
